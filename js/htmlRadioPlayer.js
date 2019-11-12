8e49977e2b0d6e5c5500a22a925647dbb3aa45e2
Showrin <showrinece15@gmail.com>
2019-11-12 23:51:05 +0600

    Control the play pause activity using Function playOrPauseFunction

--- js/htmlRadioPlayer.js
+++ js/htmlRadioPlayer.js
@@ -1,14 +1,23 @@
 $(document).ready(function() {
+    // ################ All Variables ####################
     var radioPlayPauseBtn = $('#js-play-pause-btn');
     var mainPlayer = $('#main-but-hidden-radio-player')[0];
 
 
     radioPlayPauseBtn.click(function() {
+        playOrPauseFunction(100);
+    });
+
+
+    //################# All Functions ##################### 
+
+    // ############# Function to control play or pause functionality ###########
+    function playOrPauseFunction(animationDelay = 100) {
         // ############### Scaling Animation for js-play-pause-btn ################ 
         radioPlayPauseBtn.css('transform', 'scale(.9)');
         setTimeout(function() {
             radioPlayPauseBtn.css('transform', 'scale(1)');
-        }, 100);
+        }, animationDelay);
         
         // ############### changing play & pause button with scaling animation #################
         if(radioPlayPauseBtn.children('span').hasClass('fa-play')) {
@@ -18,7 +27,7 @@ $(document).ready(function() {
                 radioPlayPauseBtn.children('span').removeClass('fa-play');
                 radioPlayPauseBtn.children('span').addClass('fa-pause');
                 radioPlayPauseBtn.children('span').css('transform', 'scale(1)');
-            }, 100);
+            }, animationDelay);
 
         } else if(radioPlayPauseBtn.children('span').hasClass('fa-pause')) {
             radioPlayPauseBtn.children('span').css('transform', 'scale(0)');
@@ -27,10 +36,10 @@ $(document).ready(function() {
                 radioPlayPauseBtn.children('span').removeClass('fa-pause');
                 radioPlayPauseBtn.children('span').addClass('fa-play');
                 radioPlayPauseBtn.children('span').css('transform', 'scale(1)');
-            }, 100);
+            }, animationDelay);
 
         }
-    });
+    }
 
     
 });

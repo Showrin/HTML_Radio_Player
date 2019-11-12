9f6174d9734266e2122dcc9b1488fcd7a0e6d637
Showrin <showrinece15@gmail.com>
2019-11-12 23:37:32 +0600

    Add play and pause animation by JS to play-pause button

---
+++ js/htmlRadioPlayer.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    var radioPlayPauseBtn = $('#js-play-pause-btn');
+
+
+    radioPlayPauseBtn.click(function() {
+        // ############### Scaling Animation for js-play-pause-btn ################ 
+        radioPlayPauseBtn.css('transform', 'scale(.9)');
+        setTimeout(function() {
+            radioPlayPauseBtn.css('transform', 'scale(1)');
+        }, 100);
+        
+        // ############### changing play & pause button with scaling animation #################
+        if(radioPlayPauseBtn.children('span').hasClass('fa-play')) {
+            radioPlayPauseBtn.children('span').css('transform', 'scale(0)');
+            setTimeout(function() {
+                radioPlayPauseBtn.children('span').removeClass('fa-play');
+                radioPlayPauseBtn.children('span').addClass('fa-pause');
+                radioPlayPauseBtn.children('span').css('transform', 'scale(1)');
+            }, 100);
+
+        } else if(radioPlayPauseBtn.children('span').hasClass('fa-pause')) {
+            radioPlayPauseBtn.children('span').css('transform', 'scale(0)');
+            setTimeout(function() {
+                radioPlayPauseBtn.children('span').removeClass('fa-pause');
+                radioPlayPauseBtn.children('span').addClass('fa-play');
+                radioPlayPauseBtn.children('span').css('transform', 'scale(1)');
+            }, 100);
+
+        }
+    });
+
+    
+});
